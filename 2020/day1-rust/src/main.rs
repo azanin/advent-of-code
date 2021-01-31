75b59883a570490992e5b79a6a19c60103563969
azanin <ale.zanin90@gmail.com>
2021-01-31 16:41:39 +0100

    wip in rust

--- 2020/day1-rust/src/main.rs
+++ 2020/day1-rust/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let report = vec![1721, 979, 366, 299, 675, 1456];
+
+
+    let duplicated: Vec<i32> = report.iter().flat_map(|&a| {
+        report.iter().flat_map(move |&b| if a + b == 2020 { vec![a * b] } else { Vec::new() })
+    }).collect();
+
+
+    println!("{:?}", duplicated);
 }
