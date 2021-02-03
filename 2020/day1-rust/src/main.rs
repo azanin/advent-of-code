961b68cff70c7a875006a09c315d9f9c3317957a
azanin <ale.zanin90@gmail.com>
2021-02-03 18:15:04 +0100

    add rust solution

--- 2020/day1-rust/src/main.rs
+++ 2020/day1-rust/src/main.rs
@@ -1,11 +1,23 @@
+use std::fs::read_to_string;
+use std::io::Error;
+use std::num::ParseIntError;
+use std::convert::TryInto;
+
+#[derive(Debug)]
+enum CliError {
+    Io(Error),
+    Parse(ParseIntError),
+}
+
 fn main() {
-    let report = vec![1721, 979, 366, 299, 675, 1456];
+    let input_values: Result<Vec<i32>, CliError> =
+        read_to_string("input.txt").map_err(CliError::Io)
+            .and_then(|s| s.lines().map(|l| l.parse::<i32>().map_err(CliError::Parse)).collect());
+
+    let results: Result<Vec<i32>, CliError> = input_values.map(|vs| vs.iter().flat_map(|&a| {
+        vs.iter().flat_map(move |&b| if a + b == 2020 { vec![a * b] } else { Vec::new() })
+    }).collect());
 
 
-    let duplicated: Vec<i32> = report.iter().flat_map(|&a| {
-        report.iter().flat_map(move |&b| if a + b == 2020 { vec![a * b] } else { Vec::new() })
-    }).collect();
-
-
-    println!("{:?}", duplicated);
+    println!("{:?}", results);
 }
