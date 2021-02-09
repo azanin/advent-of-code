00c3fea948fa1d0286e491b3e36c4099657b1730
azanin <ale.zanin90@gmail.com>
2021-02-09 17:57:46 +0100

    refactor

--- 2020/day2-rust/src/main.rs
+++ 2020/day2-rust/src/main.rs
@@ -1,13 +1,32 @@
-use std::fs::read_to_string;
-use std::ops::Deref;
 use std::error::Error;
+use std::fs::read_to_string;
 
 #[derive(Debug)]
 struct Password {
-    at_least: usize,
-    at_most: usize,
+    at_least: i32,
+    at_most: i32,
     character: char,
     value: String,
+}
+
+impl Password {
+
+    fn is_valid(&self) -> bool {
+        let occurrence =
+            self.value
+                .chars()
+                .filter(|c| *c == self.character)
+                .count();
+
+        occurrence >= self.at_least as usize && occurrence <= self.at_most as usize
+    }
+
+    fn is_valid2(&self) -> bool {
+        let char1 = self.value.chars().nth((self.at_least - 1) as usize);
+        let char2 = self.value.chars().nth((self.at_most - 1) as usize);
+
+        return (char1 == Some(self.character)) != (char2 == Some(self.character));
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -17,26 +36,23 @@ fn main() -> Result<(), Box<dyn Error>> {
             .map(|l| parse(l))
             .collect::<Result<Vec<Password>, Box<dyn Error>>>()?;
 
-    let result = passwords.iter()
-        .filter(|&password| {
-            let occurrence =
-                password.value
-                    .chars()
-                    .filter(|c| c.eq(&password.character))
-                    .count();
-            
-            occurrence >= password.at_least && occurrence <= password.at_most
-        })
+    let result1 = passwords.iter()
+        .filter(|&password| password.is_valid())
         .count();
 
-    Ok(println!("{:?}", result))
+    let result2 = passwords.iter()
+        .filter(|&password| password.is_valid2())
+        .count();
+
+
+    Ok(println!("{:?} and {:?}", result1, result2))
 }
 
 fn parse(line: &str) -> Result<Password, Box<dyn Error>> {
     let vec: Vec<&str> = line.split([' ', '-', ':'].as_ref()).collect();
 
-    let at_least = vec[0].parse::<usize>()?;
-    let at_most = vec[1].parse::<usize>()?;
+    let at_least = vec[0].parse::<i32>()?;
+    let at_most = vec[1].parse::<i32>()?;
     let character = vec[2].parse::<char>()?;
     let password_value = vec[4];
 
