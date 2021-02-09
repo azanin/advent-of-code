56d856e3768e02f8d9c80bdc7416e98f8100f953
azanin <ale.zanin90@gmail.com>
2021-02-09 16:09:48 +0100

    small refactor

--- 2020/day2-rust/src/main.rs
+++ 2020/day2-rust/src/main.rs
@@ -1,43 +1,49 @@
 use std::fs::read_to_string;
 use std::ops::Deref;
+use std::error::Error;
 
 #[derive(Debug)]
 struct Password {
     at_least: usize,
     at_most: usize,
     character: char,
-    value: String
+    value: String,
 }
 
-fn main() {
-    let input_string = read_to_string("input.txt");
+fn main() -> Result<(), Box<dyn Error>> {
+    let passwords =
+        read_to_string("input.txt")?
+            .lines()
+            .map(|l| parse(l))
+            .collect::<Result<Vec<Password>, Box<dyn Error>>>()?;
 
-    let res: Result<Vec<Password>, std::io::Error> =
-        input_string.map(|raw_string| raw_string.lines().map(|l| parse(l)).collect());
+    let result = passwords.iter()
+        .filter(|&password| {
+            let occurrence =
+                password.value
+                    .chars()
+                    .filter(|c| c.eq(&password.character))
+                    .count();
+            
+            occurrence >= password.at_least && occurrence <= password.at_most
+        })
+        .count();
 
-    let result = res.map(|passwords| passwords.iter().filter(|&p| {
-        let occurrence = p.value.chars().filter(|c| c.eq(&p.character)).count();
-        occurrence >= p.at_least && occurrence <= p.at_most
-    }).count());
-
-
-    println!("{:?}", result);
-
+    Ok(println!("{:?}", result))
 }
 
-fn parse(line: &str) -> Password {
-    let vec: Vec<&str> =
-        line.split([' ', '-', ':'].as_ref()).collect();
+fn parse(line: &str) -> Result<Password, Box<dyn Error>> {
+    let vec: Vec<&str> = line.split([' ', '-', ':'].as_ref()).collect();
 
-    let at_least = vec[0].parse::<usize>().unwrap();
-    let at_most = vec[1].parse::<usize>().unwrap();
-    let character = vec[2].parse::<char>().unwrap();
+    let at_least = vec[0].parse::<usize>()?;
+    let at_most = vec[1].parse::<usize>()?;
+    let character = vec[2].parse::<char>()?;
     let password_value = vec[4];
 
-    Password {
+    Ok(Password {
         at_least,
         at_most,
         character,
-        value: String::from(password_value)
-    }
+        value: String::from(password_value),
+    })
 }
