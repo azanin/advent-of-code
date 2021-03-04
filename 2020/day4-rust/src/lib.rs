6329caf80c816d95cd37a2624146e295718a1a37
azanin <ale.zanin90@gmail.com>
2021-03-04 10:49:58 +0100

    add rust solution

--- 2020/day4-rust/src/lib.rs
+++ 2020/day4-rust/src/lib.rs
@@ -1,23 +1,36 @@
-use std::fs::read_to_string;
-use regex::{Regex};
-use std::error::Error;
-use std::iter::Map;
+#![feature(str_split_once)]
+
 use std::collections::HashMap;
+use std::error::Error;
+use std::fs::read_to_string;
+
+use regex::Regex;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let input = read_to_string("input.txt")?;
+    let rules: Vec<(&str, fn(&str) -> bool)> =
+        vec![("byr", |str: &str| (1920..2003).contains(&str.parse::<i32>().unwrap())),
+             ("iyr", |str: &str| (2010..2021).contains(&str.parse::<i32>().unwrap())),
+             ("eyr", |str: &str| (2020..2031).contains(&str.parse::<i32>().unwrap())),
+             ("hgt", |str: &str| {
+                 return if str.ends_with("cm") && str.len() == 5 {
+                     (150..194).contains(&str[0..3].parse::<i32>().unwrap())
+                 } else if str.ends_with("in") && str.len() == 4 {
+                     (59..77).contains(&str[0..2].parse::<i32>().unwrap())
+                 } else { false };
+             }),
+             ("hcl", |str: &str| str.starts_with('#') && str.len() == 7 && str[1..].chars().all(|c| matches!(c, '0'..='9' | 'a'..='f'))),
+             ("ecl", |str: &str| vec!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].iter().any(|&v| v == str)),
+             ("pid", |str: &str| str.len() == 9 && str.chars().all(|c| c.is_digit(10)))
+        ];
 
-    let vec = parse_file_string(input.as_str())?;
+    let file = read_to_string("input.txt")?;
+    let passports_raw: Vec<&str> = parse_file_string(file.as_str())?;
+    let passports_as_maps: Vec<HashMap<&str, &str>> = passports_raw.iter().map(|&line| parse(line)).collect();
 
-    let maps: Vec<HashMap<&str, &str>> = vec.iter().map(|&line| parse(line)).collect();
+    let valid1 = passports_as_maps.iter().filter(|&pass| is_valid(pass, &rules)).count();
+    let valid2 = passports_as_maps.iter().filter(|&pass| is_valid2(pass, &rules)).count();
 
-    for el in maps {
-        for passport in el {
-            println!("<{} - {}>", passport.0, passport.1)
-        }
-    }
-
-    return Ok(());
+    return Ok(println!("{} {}", valid1, valid2));
 }
 
 fn parse_file_string(file: &str) -> Result<Vec<&str>, regex::Error> {
@@ -27,18 +40,21 @@ fn parse_file_string(file: &str) -> Result<Vec<&str>, regex::Error> {
 
 fn parse(passport_raw: &str) -> HashMap<&str, &str> {
     let fields: Vec<&str> = passport_raw.split(&['\n', ' '][..]).collect();
-    let b: Vec<Vec<&str>> = fields.iter().map(|&field| field.split(':').collect()).collect();
-    return build_map(b);
+    let b = fields.iter().filter(|&x| !x.is_empty()).map(|&field| field.split_once(':').unwrap()).collect::<HashMap<_, _>>();
+    return b;
 }
 
-fn build_map(vec: Vec<Vec<&str>>) -> HashMap<&str, &str> {
-    let mut map: HashMap<&str, &str> = HashMap::new();
-    for pair in vec {
-        map.insert(pair.first().unwrap(), pair.last().unwrap());
-    }
-    return map;
+fn is_valid(passport: &HashMap<&str, &str>, rules: &Vec<(&str, fn(&str) -> bool)>) -> bool {
+    return rules.iter().all(|&key| passport.contains_key(key.0));
 }
 
+fn is_valid2(passport: &HashMap<&str, &str>, rules: &Vec<(&str, fn(&str) -> bool)>) -> bool {
+    return rules.iter().all(|&k| {
+        let key = k.0;
+        let predicate = k.1;
+        return passport.get(key).filter(|&v| predicate(v)).is_some();
+    });
+}
 
 #[cfg(test)]
 mod tests {
@@ -55,5 +71,20 @@ mod tests {
         ]);
 
         assert_eq!(actual, expected)
+    }
+
+
+    #[test]
+    fn is_valid2_test() {
+        let input: HashMap<&str, &str> = [
+            ("key1", "koala"),
+            ("key2", "another value")
+        ].iter().cloned().collect();
+
+        let rules: Vec<(&str, fn(&str) -> bool)> = vec![("key1", |str: &str| str.contains("k"))];
+
+
+        let valid2 = is_valid2(&input, &rules);
+        assert!(valid2)
     }
 }
